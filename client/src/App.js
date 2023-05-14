f3d1480c698c584bc7ce93fc9d44684e3ab7ebd8
Basit Ali <alib4111@gmail.com>
2023-05-15 04:07:26 +0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1>Quickbooks Widget</h1>
+			<p>quickbooks widget to help to your quickbooks ledger</p>
+		</>
+	);
+}
+
+export default App;
