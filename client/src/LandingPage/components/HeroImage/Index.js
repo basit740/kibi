a4e876d647e4f21716677b9ca6d17596d0c9eee8
Basit Ali <alib4111@gmail.com>
2023-06-01 08:43:52 +0500

    image .png bug

--- client/src/LandingPage/components/HeroImage/Index.js
+++ client/src/LandingPage/components/HeroImage/Index.js
@@ -3,7 +3,7 @@ import '../../../styling/LandingPage/HeroImage.css';
 const Index = () => {
 	return (
 		<div className='hero-image'>
-			<img src='images/hero3.jpeg' alt='' />
+			<img src='images/hero3.png' alt='' />
 		</div>
 	);
 };
