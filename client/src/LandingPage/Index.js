bc4a973d778a1251a1ae7b6b6d0e13e990019efb
Basit Ali <alib4111@gmail.com>
2023-06-01 08:25:30 +0500

    video content and img remaining

--- client/src/LandingPage/Index.js
+++ client/src/LandingPage/Index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Header from './components/Header/Index';
 import HeroImage from './components/HeroImage/Index';
 import Benefits from './components/Benefits/Index';
+import DataAnalytics from './components/DataAnalytics/Index';
+import Footer from './components/Footer/Index';
+
 const Index = () => {
 	return (
 		<>
 			<Header />
 			<HeroImage />
 			<Benefits />
+			<DataAnalytics />
+			<Footer />
 		</>
 	);
 };
---
+++ client/src/LandingPage/components/DuplicateReport/Index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../../../styling/LandingPage/DuplicateReport.css';
+const Index = () => {
+	return (
+		<div className='duplicate-report'>
+			<h2 className='duplicate-report__title'>
+				The easy way to duplicate your reports
+			</h2>
+			<p className='duplicate-report__description'>
+				Simply swap the company on your template and build the same report for
+				another client in seconds
+			</p>
+			<div className='duplicate-report__actions'>
+				<a
+					href='/try'
+					className='duplicate-report__btn duplicate-report__btn--try-free'
+				>
+					Try for free
+				</a>
+				<a
+					href='/contact-us'
+					className='duplicate-report__btn duplicate-report__btn--contact-us'
+				>
+					Contact Us
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default Index;
