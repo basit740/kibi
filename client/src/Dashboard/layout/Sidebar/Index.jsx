c2e8f3ac484ecd74d469887b88781412f3a1238f
waleedk2669 <waleedk2669@gmail.com>
2023-11-22 19:46:30 +0500

    landing page responsiveness done

--- client/src/Dashboard/layout/Sidebar/Index.jsx
+++ client/src/Dashboard/layout/Sidebar/Index.jsx
@@ -1,32 +1,58 @@
-import React, { useState } from 'react';
-import styles from '../../../styling/Dashboard/Sidebar/Sidebar.module.css';
-import Menu from './Menu/Index.jsx';
-import MenuBottom from './Menu/MenuBottom/Index';
+import React, { useEffect, useState } from "react";
+import styles from "../../../styling/Dashboard/Sidebar/Sidebar.module.css";
+import Menu from "./Menu/Index.jsx";
+import MenuBottom from "./Menu/MenuBottom/Index";
 
 const Index = () => {
-	const [decrease, setDecrease] = useState(false);
-	return (
-		<div className={`${styles.sidebar} ${decrease ? styles.decrease : ''}`}>
-			<div className='sidebar__top'>
-				<div className={styles.sidebar__head}>
-					<img
-						className={styles.sidebar__logo}
-						src='/icons/sidebar-logo.png'
-						alt=''
-					/>
-					<button
-						onClick={() => setDecrease(!decrease)}
-						className={styles.sidebar__button}
-					></button>
-				</div>
-				<Menu decrease={decrease} />
-			</div>
+  const [decrease, setDecrease] = useState(false);
 
-			<div className='sidebar__bottom'>
-				<MenuBottom decrease={decrease} />
-			</div>
-		</div>
-	);
+  function isMobileDevice() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Check for mobile user agents
+    if (/windows phone/i.test(userAgent)) {
+      return true;
+    }
+
+    if (/android/i.test(userAgent)) {
+      return true;
+    }
+
+    // iOS detection
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return true;
+    }
+
+    return false;
+  }
+
+  useEffect(() => {
+    if (isMobileDevice()) {
+      setDecrease(true);
+    }
+  }, []);
+  return (
+    <div className={`${styles.sidebar} ${decrease ? styles.decrease : ""}`}>
+      <div className="sidebar__top">
+        <div className={styles.sidebar__head}>
+          <img
+            className={styles.sidebar__logo}
+            src="/icons/sidebar-logo.png"
+            alt=""
+          />
+          <button
+            onClick={() => setDecrease(!decrease)}
+            className={styles.sidebar__button}
+          ></button>
+        </div>
+        <Menu decrease={decrease} />
+      </div>
+
+      <div className="sidebar__bottom">
+        <MenuBottom decrease={decrease} />
+      </div>
+    </div>
+  );
 };
 
 export default Index;
